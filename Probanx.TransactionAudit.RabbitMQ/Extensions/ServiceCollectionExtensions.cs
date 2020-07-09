6769ea69033b1304e4fd7f95d05dcd9a256fad8f
athanassiosvoivodas <athanasios.voivodas@isignthis.com>
2020-07-09 10:33:32 +0300

    Configures api in a way that it sends messages to the queue

--- Probanx.TransactionAudit.RabbitMQ/Extensions/ServiceCollectionExtensions.cs
+++ Probanx.TransactionAudit.RabbitMQ/Extensions/ServiceCollectionExtensions.cs
@@ -2,14 +2,14 @@ using Probanx.TransactionAudit.Core.Services;
 using Probanx.TransactionAudit.Web.Services;
 using RabbitMQ.Client;
 
-namespace  Microsoft.Extensions.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
         public static IServiceCollection AddActiveMQ(this IServiceCollection serviceCollection)
         {
             return serviceCollection
-                .AddSingleton<IConnectionFactory, ConnectionFactory>()
+                .AddSingleton<IConnectionFactory>((_) => new ConnectionFactory() { HostName = "host.docker.internal" })
                 .AddSingleton<IMessageDispatcher, RabbitMqMessageDispatcher>();
         }
 
---
+++ README.md
@@ -0,0 +1,18 @@
+# TransactionAudit
+
+TransactionAudit is a tool for keeping financial transaction info for the purpose of using them for aggregation queries.
+
+## Prerequisites
+Docker and docker-compose.
+
+## Run
+
+```bash
+docker-compose up
+```
+
+## Uninstall
+
+```bash
+docker-compose down
+```
