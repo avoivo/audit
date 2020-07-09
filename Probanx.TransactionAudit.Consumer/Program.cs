b2c7de09b48e0c0d2c69bf71aa9faaa8976aef18
athanassiosvoivodas <athanasios.voivodas@isignthis.com>
2020-07-09 11:27:39 +0300

    Confingures the consumer in a way that it consumes queue messages from inside docker.

--- Probanx.TransactionAudit.Consumer/Program.cs
+++ Probanx.TransactionAudit.Consumer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -9,7 +10,10 @@ namespace Probanx.TransactionAudit.Consumer
     {
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
+            //should wait for rabbitmq to start
+            Thread.Sleep(30000);
+
+            var factory = new ConnectionFactory() { HostName = "host.docker.internal" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
@@ -30,8 +34,11 @@ namespace Probanx.TransactionAudit.Consumer
                                     autoAck: true,
                                     consumer: consumer);
 
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
+                // Console.WriteLine(" Press [enter] to exit.");
+                // Console.ReadLine();
+                Thread.Sleep(Timeout.Infinite);
+
+
             }
         }
     }
